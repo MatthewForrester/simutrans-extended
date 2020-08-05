89f0dc3e879c7d8a5022b9afc572f58696f91efc
MatthewForrester <38819845+MatthewForrester@users.noreply.github.com>
2020-08-05 14:13:19 +0100

    ADD: Bash script for handling multiple compilation targets

---
+++ simumake.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+target=$1
+configuration_file="config."
+
+if [ "-n $target" ]
+then
+    echo -e "Missing configuration parameter. \n Please add the name of the configuration file you wish to use. \n For example, if your configuration file is config.linux, then enter 'simumake linux'"
+else
+
+    configuration_file="${configuration_file} ${target}"
+    echo "Configuration target is ${configuration.file}"
+
+        if [ "! -s config.linux"];
+        then
+            echo "Compilation failed: could not find Linux configuration file."
+        else 
+
+            cp -f config.default config.default.tmp
+
+            if [ "! -s config.default.tmp" -o "config.default.tmp -older 1m" ];
+            then
+                echo "Compilation failed: could not backup config.default."	
+            else
+                echo "Config.default backed up."
+        
+                cp -f config.linux config.default
+        
+                if [ "! -s config.default" -o "config.default.tmp -older 1m" ];
+                then
+                    echo "Compilation failed: could not copy compilation configuration file"
+                else
+                    echo "Configuration copied."
+
+
+                    branch=$(git branch --show-current)
+
+                    echo $branch
+
+                    # make -j2
+
+                fi
+
+            fi
+
+        fi    
+
+fi
+
+
+
